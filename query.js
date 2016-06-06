1038ca37a3fd380941386c13aa02a7932d5b2e10
Sennsei <isaacsenn@gmail.com>
2016-06-06 12:46:57 +1000

    Create query.js

---
+++ query.js
@@ -0,0 +1,28 @@
+/*********************************************
+*** Plug.dj Script - Username to ID Query
+***
+***  Author:       brotherband
+***  Contact:      ....
+***  Version:      1.1
+***  Created:      5/6/16
+***  Last Updated: 6/6/16
+***
+***
+*** (c) 2016 Sennsei
+*** Protected under the MIT License
+*********************************************/
+
+var userQuery = prompt("\nPlease enter the username you would like to apply the ID Lookup on!\n     + Please Note: This query is capital sensitive!\n", "Example: brotherband");
+var usersOnline = API.getUsers(); 
+
+for (var userList = 0; userList < usersOnline.length; userList++) {
+	if (userQuery === null) {
+		console.log('No input recieved.');
+	} else if (usersOnline[userList].username == userQuery) {
+		prompt("\n" + userQuery + "'s ID is:", usersOnline[userList].id);
+		break;
+	} else {
+		alert("\n" + userQuery + " is not currently in this room.\n");
+		break;
+	}
+}
