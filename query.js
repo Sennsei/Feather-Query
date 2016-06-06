36f5352a6c919cc003241611ef6417a46acf1cfb
Sennsei <isaacsenn@gmail.com>
2016-06-06 16:09:40 +1000

    Update query.js

--- query.js
+++ query.js
@@ -1,5 +1,5 @@
 /*********************************************
-*** Plug.dj Script - Username to ID Query
+***  Plug.dj Script - Username to ID Query
 ***
 ***  Author:       brotherband
 ***  Contact:      ....
@@ -8,21 +8,22 @@
 ***  Last Updated: 6/6/16
 ***
 ***
-*** (c) 2016 Sennsei
-*** Protected under the MIT License
+***  (c) 2016 Sennsei
+***  Protected under the MIT License
 *********************************************/
 
-var userQuery = prompt("\nPlease enter the username you would like to apply the ID Lookup on!\n     + Please Note: This query is capital sensitive!\n", "Example: brotherband");
-var usersOnline = API.getUsers(); 
+var userQuery = prompt("\nPlease enter the username you would like to apply the ID Lookup on!\n     + Please Note: This query is capital sensitive!\n", "Example: brotherband").toUpperCase();
+var usersInit = API.getUsers();
+var usersOnline = usersInit.toUpperCase();
 
 for (var userList = 0; userList < usersOnline.length; userList++) {
-	if (userQuery === null) {
-		console.log('No input recieved.');
-	} else if (usersOnline[userList].username == userQuery) {
-		prompt("\n" + userQuery + "'s ID is:", usersOnline[userList].id);
-		break;
-	} else {
-		alert("\n" + userQuery + " is not currently in this room.\n");
-		break;
+	if (userQuery !== null) {
+		if (usersOnline[userList].username == userQuery) {
+			prompt("\n" + userQuery + "'s ID is:", usersOnline[userList].id);
+			break;
+		} else {
+			alert("\n" + userQuery + " is not currently in this room.\n");
+			break;
+		}
 	}
 }
