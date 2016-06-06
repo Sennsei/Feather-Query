e2a1918ee47e57b7d715e2dbbd680db67621081f
Sennsei <isaacsenn@gmail.com>
2016-06-06 16:12:16 +1000

    Update query.js

--- query.js
+++ query.js
@@ -17,13 +17,13 @@ var usersInit = API.getUsers();
 var usersOnline = usersInit.toUpperCase();
 
 for (var userList = 0; userList < usersOnline.length; userList++) {
-	if (userQuery !== null) {
-		if (usersOnline[userList].username == userQuery) {
-			prompt("\n" + userQuery + "'s ID is:", usersOnline[userList].id);
-			break;
-		} else {
-			alert("\n" + userQuery + " is not currently in this room.\n");
-			break;
-		}
+	if (userQuery ==  null) {
+		console.log('No input recieved.')
+	} else if (usersOnline[userList].username == userQuery) {
+		prompt("\n" + userQuery + "'s ID is:", usersOnline[userList].id);
+		break;
+	} else {
+		alert("\n" + userQuery + " is not currently in this room.\n");
+		break;
 	}
 }
